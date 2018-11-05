e06ff77b2daa5a36c0ac1310db3cd91aec2f3c31
Nate Walters <newsproutsmedia@gmail.com>
2018-11-05 13:07:41 -0800

    First and last commit

--- src/main/java/com/in28minutes/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.currencyexchangeservice.controllers;
 
 import com.in28minutes.currencyexchangeservice.domain.ExchangeValue;
 import com.in28minutes.currencyexchangeservice.repositories.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ public class CurrencyExchangeController {
 
     private ExchangeValueRepository exchangeValueRepository;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public CurrencyExchangeController(Environment environment, ExchangeValueRepository exchangeValueRepository) {
         this.environment = environment;
         this.exchangeValueRepository = exchangeValueRepository;
@@ -24,6 +28,9 @@ public class CurrencyExchangeController {
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+
+        logger.info("{}", exchangeValue);
+
         return exchangeValue;
     }
 }
